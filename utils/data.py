decadbc3b1bed83d442395ad4f616c81ef44c814
my-my-l <2430182886@qq.com>
2024-02-07 21:14:21 +0800

    first commit

--- utils/data.py
+++ utils/data.py
@@ -5,6 +5,73 @@ from timm.data.auto_augment import auto_augment_transform, rand_augment_transfor
 import tllib.vision.datasets as datasets
 from tllib.vision.transforms import ResizeImage
 from torch.utils.data import DataLoader
+import os
+from typing import Optional, Callable, Tuple, List
+from torchvision.datasets.folder import default_loader
+
+
+class ImageList(datasets.VisionDataset):
+    
+    def __init__(self, root: str, classes: List[str], data_list_file: str,
+                 transform: Optional[Callable] = None, target_transform: Optional[Callable] = None):
+        super().__init__(root, transform=transform, target_transform=target_transform)
+        self.classes = classes
+        self.samples = self.multi_labels_parse_data_file(data_list_file)
+        self.samples = self.parse_data_file(data_list_file)
+        self.targets = [s[1] for s in self.samples]
+        self.class_to_idx = {cls: idx
+                             for idx, cls in enumerate(self.classes)}
+        self.loader = default_loader
+        self.data_list_file = data_list_file
+
+
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+            return (tuple): (image, target) where target is index of the target class.
+        """
+        path, target = self.samples[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None and target is not None:
+            target = self.target_transform(target)
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+    def parse_data_file(self, file_name: str) -> List[Tuple[str, int]]:
+        """Parse file to data list
+
+        Args:
+            file_name (str): The path of data file
+            return (list): List of (image path, class_index) tuples
+        """
+        with open(file_name, "r") as f:
+            data_list = []
+            for line in f.readlines():
+                split_line = line.split()
+                target = split_line[-1]
+                path = ' '.join(split_line[:-1])
+                if not os.path.isabs(path):
+                    path = os.path.join(self.root, path)
+                target = int(target)
+                data_list.append((path, target))
+        return data_list
+
+
+    @property
+    def num_classes(self) -> int:
+        """Number of classes"""
+        return len(self.classes)
+
+    @classmethod
+    def domains(cls):
+        """All possible domain in this dataset"""
+        raise NotImplemented
+
 
 
 class MedicalImages(ImageList):
